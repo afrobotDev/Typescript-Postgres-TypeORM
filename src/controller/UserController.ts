d34da80c5b342be55602bd993652bdbe12eb895d
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-19 07:46:54 +0300

    Refactor UserController to use instance-level repository getter

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -2,15 +2,17 @@ import { Request, Response } from "express";
 import { AppDataSource } from "../config/data-source.js";
 import { User } from "../entity/User.js";
 
-const userRepository = AppDataSource.getRepository(User);
-
 export class UserController {
+  private get userRepository() {
+    return AppDataSource.getRepository(User);
+  }
+
   async all(_req: Request, _res: Response) {
-    return userRepository.find();
+    return this.userRepository.find();
   }
 
   async one(req: Request, res: Response) {
-    const user = await userRepository.findOneBy({ id: Number(req.params.id) });
+    const user = await this.userRepository.findOneBy({ id: Number(req.params.id) });
     if (!user) {
       res.status(404).json({ message: "User not found" });
       return;
@@ -19,18 +21,18 @@ export class UserController {
   }
 
   async save(req: Request, _res: Response) {
-    const user = userRepository.create(req.body);
-    await userRepository.save(user);
+    const user = this.userRepository.create(req.body);
+    await this.userRepository.save(user);
     return user;
   }
 
   async remove(req: Request, res: Response) {
-    const user = await userRepository.findOneBy({ id: Number(req.params.id) });
+    const user = await this.userRepository.findOneBy({ id: Number(req.params.id) });
     if (!user) {
       res.status(404).json({ message: "User not found" });
       return;
     }
-    await userRepository.remove(user);
+    await this.userRepository.remove(user);
     return { message: "User deleted" };
   }
 }
